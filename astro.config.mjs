0219ad845fd6800137b3125012120794bf46c5da
Harald von Waldow <harald.vonwaldow@thuenen.de>
2024-10-24 18:07:59 +0200

    set site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://research-data-management.github.io/research-software-licensing/'
   integrations: [
     starlight({
       title: "Research software licensing guide",
