a30917b662835f90edd6fda15798302af91d3af0
Harald von Waldow <harald.vonwaldow@thuenen.de>
2024-10-24 15:27:18 +0200

    remove TI logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Research software licensing guide",
-      logo: {
-        src: './src/assets/THUENEN_SCREEN_logoonly_RGB.png',
-        alt: "Logo of the Thünen Institute",
-      },
       customCss: [
         './src/styles/custom.css',
       ],
