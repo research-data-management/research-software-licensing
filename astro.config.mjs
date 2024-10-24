1f4becca5c4795c8931214018285f28a2639865e
Harald von Waldow <harald.vonwaldow@thuenen.de>
2024-10-24 18:10:50 +0200

    fix typo in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://research-data-management.github.io/research-software-licensing/'
+  site: 'https://research-data-management.github.io/research-software-licensing/',
   integrations: [
     starlight({
       title: "Research software licensing guide",
